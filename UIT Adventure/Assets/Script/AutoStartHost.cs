c381ce1818916a97d5159a77b2bd52bfab82d76c
Kiet <kietngo255@gmail.com>
2023-05-21 17:18:01 +0700

    last commit, bye this branch, move to network1

--- UIT Adventure/Assets/Script/AutoStartHost.cs
+++ UIT Adventure/Assets/Script/AutoStartHost.cs
@@ -6,15 +6,17 @@ using Unity.Netcode;
 public class AutoStartHost : MonoBehaviour
 {
     public NetworkManager networkManager;
+    
     private void Start()
     {
-        
-
         if (networkManager == null)
         {
             Debug.LogError("NetworkManager component is missing. Please attach the NetworkManager component to the same GameObject.");
             return;
         }
+
+        GameObject networkManagerObj = networkManager.gameObject;
+        DontDestroyOnLoad(networkManagerObj); // Mark the NetworkManager object as "Don't Destroy On Load"
 
         if (NetworkManager.Singleton.IsHost)
         {
